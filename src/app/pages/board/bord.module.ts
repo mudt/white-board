7d2742e23a6ac7378fa1eb8bdaadf5135ad10b03
mudt <mueharadt@gmail.com>
2020-04-29 16:29:53 +0900

    fix: ruting設定が足りていなかったため追加

--- src/app/pages/board/bord.module.ts
+++ src/app/pages/board/bord.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board/board.component';
 import { CardComponent } from './card/card.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
+import { BoardRoutingRoutingModule } from './board-routing-routing.module';
 
 @NgModule({
   declarations: [BoardComponent, CardComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, BoardRoutingRoutingModule],
 })
 export class BordModule {}
